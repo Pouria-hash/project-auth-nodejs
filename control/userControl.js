747819b0b5b15ca996a40d8ef7ce73a215e3ed05
Pouria-hash <75394702+Pouria-hash@users.noreply.github.com>
2021-12-13 14:06:36 +0330

    two

--- control/userControl.js
+++ control/userControl.js
@@ -1,35 +1,34 @@
 const User = require('../models/user');
 const passport = require('passport');
 
-
 module.exports.registerForm = (req, res) => {
-    res.render('user/register')
-}
+	res.render('user/register');
+};
 
 module.exports.registr = async (req, res) => {
-    const { username, email, password } = req.body;
-    const u = new User({ username, email })
-    const registerUser = await User.register(u, password)
-    req.logIn(registerUser, err => {
-        if (err) return next(err)
-    })
-    req.flash('success', 'Wellcome')
-    res.redirect('/painting')
-}
+	const { username, email, password } = req.body;
+	const u = new User({ username, email });
+	const registerUser = await User.register(u, password);
+	req.logIn(registerUser, (err) => {
+		if (err) return next(err);
+	});
+	req.flash('success', 'Wellcome');
+	res.redirect('/');
+};
 
 module.exports.loginForm = (req, res) => {
-    res.render('user/login')
-}
+	res.render('user/login');
+};
 
 module.exports.loginUser = (req, res) => {
-    const foundPath = req.session.returnTo || '/painting'
-    delete req.session.returnTo
-    req.flash('success', 'Wellcome Back')
-    res.redirect(foundPath)
-}
+	const foundPath = req.session.returnTo || '/';
+	delete req.session.returnTo;
+	req.flash('success', 'Wellcome Back');
+	res.redirect(foundPath);
+};
 
 module.exports.logoutUser = (req, res) => {
-    req.logOut()
-    req.flash('success', 'GoodBye')
-    res.redirect('/painting')
-}
+	req.logOut();
+	req.flash('success', 'GoodBye');
+	res.redirect('/');
+};
